f46784a597db45028804ff509475a7b91ef2ece3
League Student <league@iMac-9.attlocal.net>
2019-10-18 18:28:22 -0700

    a few errors that were fixed we learned about inputs and messages

--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -5,13 +5,13 @@ import javax.swing.JOptionPane;
 public class CandyCrush {
 
 	public static void main(String[] args) {
-		// 1. Using a pop-up, ask the user who they don't like
+	 String name= JOptionPane.showInputDialog("What is your name?");
 
 		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
-		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
-		// 3. Ask the user for the name of their best friend
+		JOptionPane.showMessageDialog(null, "You totally have a crush on spongebob!");
+		JOptionPane.showInputDialog("Who is your best friend?" );
 
-		// 4. Tell them their best friend is as sweet as candy
+	JOptionPane.showMessageDialog(null,"Your best Friend is as sweet as Candy");
 
 	} 
 }
